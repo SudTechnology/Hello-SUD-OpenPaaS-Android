1a429178dace3bb20ff19f642a13208fb4c96577
liwensi <liwensi@sud.tech>
2026-03-15 21:55:25 +0800

    feat：优化目录选择

--- project/StartGameByDir/src/main/java/tech/sud/mgp/hello/ui/main/MainActivity.java
+++ project/StartGameByDir/src/main/java/tech/sud/mgp/hello/ui/main/MainActivity.java
@@ -7,14 +7,12 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.Settings;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
-
-import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.ToastUtils;
-import com.blankj.utilcode.util.UriUtils;
 import com.codekidlabs.storagechooser.StorageChooser;
 
 import java.io.File;
@@ -28,10 +26,9 @@ import tech.sud.mgp.hello.ui.game.QuickStartGameActivity;
  * 主页
  */
 public class MainActivity extends BaseActivity {
-    private static final int _REQUEST_CODE_PICK_JSON = 20001;
 
-    private File gamePkgDir;
     private TextView tvInfo;
+    private EditText etPath;
 
     @Override
     protected int getLayoutId() {
@@ -42,6 +39,7 @@ public class MainActivity extends BaseActivity {
     protected void initWidget() {
         super.initWidget();
         tvInfo = findViewById(R.id.tv_info);
+        etPath = findViewById(R.id.et_path);
         initPermission();
     }
 
@@ -87,28 +85,27 @@ public class MainActivity extends BaseActivity {
         });
     }
 
-    private void onClickStart() {
-        if (gamePkgDir == null) {
-            ToastUtils.showShort("请先选择游戏包目录");
-            return;
-        }
-
-        GameModel gameModel = new GameModel();
-        gameModel.gameName = gamePkgDir.getParentFile().getName();
-        gameModel.gameId = gameModel.gameName;
-        gameModel.gamePkgVersion = "1.0.0";
-        gameModel.gameUrl = gamePkgDir.getAbsolutePath();
-        gameModel.pathType = SUDOPGamePathType.DIR;
-//        gameModel.gameUrl = "/sdcard/Download/aqua/app";
-        gameModel.gameUrl = "/sdcard/Download/hayypmaster25.oppo.nearme.gamecente";
-        QuickStartGameActivity.start(this, gameModel);
-    }
-
     private void onClickSelect() {
-//        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-//        intent.addCategory(Intent.CATEGORY_OPENABLE);
-//        intent.setType("*/*");
-//        startActivityForResult(intent, _REQUEST_CODE_PICK_JSON);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            ToastUtils.showLong("当前目录选择器不支持高于Android10的版本，请手动输入");
+            return;
+        } else {
+            String[] permissions = new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            };
+            boolean isGranted = true;
+            for (String permission : permissions) {
+                if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                    isGranted = false;
+                    break;
+                }
+            }
+            if (!isGranted) {
+                ToastUtils.showShort("没有拿到权限");
+                return;
+            }
+        }
 
         StorageChooser chooser = new StorageChooser.Builder()
                 .withActivity(this)
@@ -117,43 +114,40 @@ public class MainActivity extends BaseActivity {
 //                .withPredefinedPath(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath())
                 .setType(StorageChooser.DIRECTORY_CHOOSER)
                 .build();
-
         chooser.setOnSelectListener(path -> {
             if (TextUtils.isEmpty(path)) {
                 return;
             }
-            File gamePkgDir = new File(path);
-            if (!gamePkgDir.exists()) {
-                ToastUtils.showShort("游戏包目录不存在");
-                return;
-            }
-            this.gamePkgDir = gamePkgDir;
-            tvInfo.setText("当前选择运行的游戏包路径为:" + gamePkgDir);
+            etPath.setText(path);
         });
-
         chooser.show();
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode != _REQUEST_CODE_PICK_JSON || resultCode != RESULT_OK || data == null) {
+    private void onClickStart() {
+        String path = getInputPath();
+        File gameDir;
+        if (TextUtils.isEmpty(path) || !(gameDir = new File(path)).exists()) {
+            ToastUtils.showShort("游戏目录不存在");
             return;
         }
-        Uri uri = data.getData();
-        if (uri == null) {
-            return;
+
+        GameModel gameModel = new GameModel();
+        gameModel.gameName = gameDir.getParentFile().getName();
+        gameModel.gameId = gameModel.gameName;
+        gameModel.gamePkgVersion = "1.0.0";
+        gameModel.gameUrl = gameDir.getAbsolutePath();
+        gameModel.pathType = SUDOPGamePathType.DIR;
+//        gameModel.gameUrl = "/sdcard/Download/aqua/app";
+        QuickStartGameActivity.start(this, gameModel);
+    }
+
+    private String getInputPath() {
+        Editable text = etPath.getText();
+        if (text == null) {
+            return null;
         }
-        File file = UriUtils.uri2File(uri);
-        File gamePkgDir = file.getParentFile();
-        LogUtils.d("file:" + file);
-        if (gamePkgDir == null || !gamePkgDir.exists()) {
-            ToastUtils.showShort("游戏包目录不存在");
-            return;
-        }
-        this.gamePkgDir = gamePkgDir;
-        tvInfo.setText("当前选择运行的游戏包路径为:" + gamePkgDir);
+        return text.toString();
     }
 
 }
